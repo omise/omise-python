3ad9a5cd477ef0c9c9e11885893269e4c98af3a5
bank <jakkrij@omise.co>
2017-06-08 16:02:44 +0700

    Add description

--- omise/__init__.py
+++ omise/__init__.py
@@ -1355,6 +1355,28 @@ class Refund(_MainResource, Base):
 
 
 class Search(_MainResource, Base):
+    """API class for searching.
+
+    This API class is used for retrieving results from your account.
+    Currently, Search API is supported to search only Charge, Dispute,
+    Recipient and Customer.
+
+    Basic usage::
+
+        >>> import omise
+        >>> omise.api_secret = 'skey_test_4xsjvwfnvb2g0l81sjz'
+        >>> search = omise.Search.execute('charge', {
+            "query": "thb",
+            "filters": {
+                "amount": "1000..2000",
+                "captured": 'true'
+            }
+        })
+        <Search at 0x1029e57f0>
+        >>> search[0]
+        <Charge id='chrg_test_58505fmz8hbaln3283s' at 0x10291edd8>
+    """
+
     def __len__(self):
         return len(self._attributes['data'])
 
