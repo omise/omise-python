dc41d7d5c7939a9917fa28a507082c6e8db5ee70
Daniel Fowler <daniel@omise.co>
2020-08-06 16:06:41 +0700

    Rely on default path assumption for io.open

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='omise',
