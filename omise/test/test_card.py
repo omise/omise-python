ce23ff600c5864d2cff55c4f2c0dc5aa5dc73028
Daniel Fowler <daniel@omise.co>
2020-08-06 14:57:39 +0700

    Add test for one-line card retrieval

--- omise/test/test_card.py
+++ omise/test/test_card.py
@@ -64,6 +64,34 @@ class CardTest(_ResourceMixin, unittest.TestCase):
             'https://api.omise.co/customers/cust_test/cards/card_test'
         )
 
+    @mock.patch('requests.get')
+    def test_retrieve(self, api_call):
+        class_ = self._getTargetClass()
+
+        self.mockResponse(api_call, """{
+            "object": "card",
+            "id": "card_test",
+            "livemode": false,
+            "location": "/customers/cust_test/cards/card_test",
+            "country": "",
+            "city": "Bangkok",
+            "postal_code": "10310",
+            "financing": "",
+            "last_digits": "4242",
+            "brand": "Visa",
+            "expiration_month": 12,
+            "expiration_year": 2018,
+            "fingerprint": "098f6bcd4621d373cade4e832627b4f6",
+            "name": "Somchai Prasert",
+            "created": "2014-10-21T04:04:12Z"
+        }""")
+
+        card = class_.retrieve('cust_test', 'card_test')
+        self.assertTrue(isinstance(card, class_))
+        self.assertEqual(card.id, 'card_test')
+        self.assertEqual(card.name, 'Somchai Prasert')
+        self.assertRequest(api_call, 'https://api.omise.co/customers/cust_test/cards/card_test')
+
     @mock.patch('requests.patch')
     def test_update(self, api_call):
         card = self._makeOne()
