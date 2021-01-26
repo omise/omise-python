e5230dd355d2ddb296ce80e21ff26b8795ac6251
Daniel Fowler <daniel@omise.co>
2021-01-26 16:38:05 +0700

    Add testing for 3.9

--- setup.py
+++ setup.py
@@ -38,5 +38,6 @@ setup(name='omise',
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
           "Topic :: Software Development :: Libraries :: Python Modules",
       ])
